545a8f2b3d13bdb2de123c9630f3add11ea462e6
Laurenz Weixlbaumer <root@fsoc.space>
2024-05-22 20:46:34 +0200

    add api types

--- src/store.ts
+++ src/store.ts
@@ -1,3 +1,19 @@
 import { signal } from '@preact/signals';
 
-export const itemData;
+export interface Item {
+    id: string;
+    shortName: string;
+    iconLink: string;
+    width: number;
+    height: number;
+    types: string[];
+    lastLowPrice: number;
+    historicalPrices: {
+        price: number;
+        timestamp: number;
+    }[];
+}
+
+export type ItemData = Map<Item['id'], Item>;
+
+export const itemData = signal<ItemData | null>(null);
