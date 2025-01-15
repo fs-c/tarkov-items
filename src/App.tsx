cf1e616e807fc2cd12d7ad91bc805dfba18f8a1d
fsoc <root@fsoc.space>
2025-01-15 23:30:53 +0100

    Initialize stores as soon as relevant fetchers are done

--- src/App.tsx
+++ src/App.tsx
@@ -18,35 +18,33 @@ import { LootSpawnsMap } from './components/LootSpawnsMap';
 import { fetchAllMapMetadata } from './fetcher/fetch-map-metadata';
 
 export function App() {
+    // fetch all data on app load, components should implement loading states as needed
     useMemo(() => {
-        Promise.all([
-            fetchTranslations(),
-            fetchStaticSpawnsPerMap(),
-            fetchContainerContentPerMap(),
-            fetchAllItemMetadata(),
-            fetchLooseLootPerMap(),
-            fetchAllMapMetadata(),
-        ])
-            .then(
-                ([
-                    translationsValue,
-                    staticSpawnsPerMapValue,
-                    containerContentPerMapValue,
-                    allItemMetadataValue,
-                    looseLootPerMapValue,
-                    allMapMetadataValue,
-                ]) => {
-                    translations.value = translationsValue;
-                    staticSpawnsPerMap.value = staticSpawnsPerMapValue;
-                    containerContentPerMap.value = containerContentPerMapValue;
-                    allItemMetadata.value = allItemMetadataValue;
-                    looseLootPerMap.value = looseLootPerMapValue;
-                    allMapMetadata.value = allMapMetadataValue;
-                },
-            )
-            .catch((err: unknown) => {
-                console.error(err);
-            });
+        // todo: these should really have some kind of error handling/maybe retries?
+
+        void fetchTranslations().then((translationsValue) => {
+            translations.value = translationsValue;
+        });
+
+        void fetchStaticSpawnsPerMap().then((staticSpawnsPerMapValue) => {
+            staticSpawnsPerMap.value = staticSpawnsPerMapValue;
+        });
+
+        void fetchContainerContentPerMap().then((containerContentPerMapValue) => {
+            containerContentPerMap.value = containerContentPerMapValue;
+        });
+
+        void fetchAllItemMetadata().then((allItemMetadataValue) => {
+            allItemMetadata.value = allItemMetadataValue;
+        });
+
+        void fetchLooseLootPerMap().then((looseLootPerMapValue) => {
+            looseLootPerMap.value = looseLootPerMapValue;
+        });
+
+        void fetchAllMapMetadata().then((allMapMetadataValue) => {
+            allMapMetadata.value = allMapMetadataValue;
+        });
     }, []);
 
     return (
